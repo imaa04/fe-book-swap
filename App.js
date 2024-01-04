a38adb42d6c0036b7831510ae9fe1f3147e29c53
imaa04 <imakhan.04@gmail.com>
2024-01-04 13:40:01 +0000

    create book swap app using react native

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex flex-1 bg-yellow-100 justify-center items-center">
+      <Text className='font-bold text-5xl'>Book Swap!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
