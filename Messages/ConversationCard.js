85d49d4b7a00c3eaf0ed6802b8ca375da6eab582
Tom Bryson <tom@Toms-Mac-mini-2.lan>
2024-01-09 12:10:18 +0000

    changes to ws connection

--- Messages/ConversationCard.js
+++ Messages/ConversationCard.js
@@ -4,48 +4,55 @@ import { getMessages } from "./api";
 import MessageInput from "./MessageInput";
 import * as React from "react";
 
+export default ConversationCard = ({ route, navigation }) => {
+  const user = "Sarah Blue";
+  const [webSocketReady, setWebSocketReady] = useState(false);
 
-export default ConversationCard = ({route, navigation}) => {
-
-
-  const socket = useRef(null);
+  const [webSocket, setWebSocket] = useState(
+    new WebSocket(`ws://localhost:3000/${user}`)
+  );
 
   useEffect(() => {
-    
-     socket.current = new WebSocket(
-      "ws://localhost:3000/Sarah Blue"
-    );
-    socket.current.onopen = () => {console.log("connection opened")}
-    socket.current.onclose = () => {console.log("connection closed")}
- 
-    const wsCurrent = socket.current;
-
-    return () => {
-      wsCurrent.close();
+    webSocket.onopen = (event) => {
+      console.log("connected to WSS");
+      setWebSocketReady(true);
     };
 
+    webSocket.onclose = function (event) {
+      console.log("disconnected");
+      setWebSocketReady(false);
+      setTimeout(() => {
+        setWebSocket(new WebSocket("ws://localhost:3000"));
+      }, 1000);
+    };
 
+    webSocket.onerror = function (err) {
+      console.log("Socket encountered error: ", err.message, "Closing socket");
+      setWebSocketReady(false);
+      webSocket.close();
+    };
+
+    // request to api
+
+    return () => {
+      webSocket.close();
+    };
+  }, [webSocket]);
+
+  const [messages, setMessages] = useState([]);
+  const { conversationWith } = route.params;
+
+  useEffect(() => {
+    getMessages(user, conversationWith).then(({ data }) => {
+      setMessages(data.messages);
+    });
   }, []);
-
-
-  const user = "Sarah Blue"
-
-  const [messages, setMessages] = useState([])
-  const {conversationWith} = route.params
-
-    useEffect(() => {
-      getMessages(user, conversationWith ).then(({ data }) => {
-        setMessages(data.messages);
-      });
-    }, []);
-
 
   const testMessage = {
     between: ["Sarah Blue", "David Black"],
     from: "Sarah Blue",
     body: "hardcoded message from front end",
   };
-
 
   return (
     <View>
@@ -67,8 +74,7 @@ export default ConversationCard = ({route, navigation}) => {
         setMessages={setMessages}
         conversationWith={conversationWith}
       />
-      <Pressable
-      >
+      <Pressable>
         <Text>Send Test Message</Text>
       </Pressable>
     </View>
